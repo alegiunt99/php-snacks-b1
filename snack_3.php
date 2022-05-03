c7c132e00fe7cb361bfc6c2e7cb9650ffcdb81f4
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-03 17:30:23 +0200

    Provo ad usare il ciclo while

--- snack_3.php
+++ snack_3.php
@@ -1,3 +1,20 @@
 <!-- Creare un array con 15 numeri casuali, 
 tenendo conto che l’array non dovrà contenere 
 lo stesso numero più di una volta -->
+
+<?php
+
+$numbersGroup = [];
+
+$randomNumbers = rand(1, 100)
+
+while (count($numbersGroup) < 15){
+
+    $numbersGroup[] = $randomNumbers;
+
+    echo($numbersGroup);
+
+}
+
+echo($numbersGroup);
+?>
