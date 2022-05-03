e8f711b82460d828e04aa1f4dcefa3e13833e2f0
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-03 16:13:53 +0200

    Creo l'array con tutte le partite del calendario

--- snack_1.php
+++ snack_1.php
@@ -1,4 +1,103 @@
 <!-- 
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, 
+punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
  -->
+
+ <?php
+ 
+ $calendarMatch = [
+
+    [
+        "home_team" => "Virtus Bologna",
+
+        "away_team" => "Olimpia Milano",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Brescia",
+
+        "away_team" => "Venezia",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Derthona",
+
+        "away_team" => "Dinamo Sassari",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Reggiana",
+
+        "away_team" => "VL Pesaro",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Trieste",
+
+        "away_team" => "Brindisi",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Varese",
+
+        "away_team" => "Universo Treviso",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Trento",
+
+        "away_team" => "Napoli",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+    [
+        "home_team" => "Fortitudo Bologna",
+
+        "away_team" => "Cremona",
+
+        "home_score" => rand(25, 90),
+
+        "away_score" => rand(25, 90)
+
+    ],
+
+ ]
+
+ ?>
