c4036bcebc97d0b849852d5132d88574ac73c370
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-03 18:16:07 +0200

    Ho diviso il paragrafo in piccoli paragrafi... ...per ogni punto

--- snack_4.php
+++ snack_4.php
@@ -1,3 +1,32 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
 Prendere il paragrafo e suddividerlo in tanti paragrafi.
 Ogni punto un nuovo paragrafo. -->
+
+<?php
+
+$paragraph = '
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ipsum lorem,
+aliquet sit amet porttitor vitae, posuere a felis. Vestibulum feugiat lorem placerat leo imperdiet,
+non tempor mi sodales. Ut eget lorem quis ligula hendrerit ornare. Aliquam at lacinia ipsum. Aenean mi nulla,
+bibendum at porttitor lobortis, posuere id libero. Nam vitae lacinia enim. Vestibulum fermentum metus felis,ù
+sit amet euismod justo sollicitudin convallis. Donec semper neque maximus, bibendum dolor et, condimentum sapien.
+Proin libero eros, convallis eu enim sit amet, rutrum blandit massa. Curabitur condimentum at ante id ornare. In purus elit,
+commodo vel vulputate quis, hendrerit quis arcu. Nullam a elit massa. Donec ac aliquet odio, quis vehicula lorem.
+';
+
+$newParagraph = explode('.', $paragraph);
+
+?>
+
+<p>
+    <?php
+       echo($paragraph);
+    ?>
+</p>
+
+
+<pre>
+    <?php
+       var_dump($newParagraph);
+    ?>
+</pre>
